9dfde108dc5159a6c3e4b963eaf903f1a179dd2d
vinithp <36687795+vinithp@users.noreply.github.com>
2021-01-11 12:46:08 +0530

    Update shh.py

--- shh.py
+++ shh.py
@@ -1,2 +1,4 @@
-get_ipython().system_raw('echo bang')
-print("gang")
+from colab_ssh import launch_ssh_cloudflared, init_git_cloudflared
+get_ipython().system_raw('pip install colab_ssh --upgrade')
+launch_ssh_cloudflared(password="colab12")
+print("password is colab12")
